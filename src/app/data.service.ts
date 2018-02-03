5a6b45488cab4ac0c966f7e0f3b13dda6b15ae05
Dipak Kumar Nimawat <dipak.nimawat@gmail.com>
2018-02-04 00:05:40 +0530

    registration form

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -17,5 +17,12 @@ export class DataService {
     const body = new HttpParams().set('action', 'login').set('user_name', user_name).set('password', password);
     return this._http.post(environment.api_url, body, { headers: myheader });
   }
+  userRegister(userRegi){
+    const myheader = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    const body = new HttpParams().set('action','registration').set('name', 'name').set('email','email').set('mobile','mobile').set('password',JSON.stringify(userRegi));
+    return this._http.post(environment.api_url, body,{headers:myheader });
+  }
+
+
 
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
+import {ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  private myform : FormGroup;
+  private fname : FormControl;
+  private lname : FormControl;
+  private email : FormControl;
+  private password: FormControl;
+  private mobile : FormControl;
+  constructor( private data_service : DataService, public Router: Router) { }
 
   ngOnInit() {
+    this.createRegisterationForm();
+    this.createRegisterationFormControls();
   }
+
+  getRegister(){
+    this.data_service.userRegister(this.myform.value.fname).subscribe(result=>{
+   // this.data_service.userRegister(this.myform.value.name,this.myform.value.email,this.myform.value.password,this.myform.value.mobile).subscribe(result=>{
+    console.log(result);
+    })
+  }
+
+  createRegisterationForm(){
+    this.fname= new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+    ]);
+    this.lname= new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+    ]);
+    this.email = new FormControl('',[
+      Validators.required,
+      Validators.minLength(1),
+    ]);
+    this.password = new FormControl('',[
+      Validators.required,
+      Validators.minLength(1),
+    ]);
+    this.mobile = new FormControl('',[
+      Validators.required,
+      Validators.minLength(1),
+    ]);
+
+  }
+
+  createRegisterationFormControls(){
+    this.myform = new FormGroup({
+      fname: this.fname,
+      lname: this.lname,
+      email:this.email,
+      password:this.password,
+      mobile:this.mobile
+
+    });
+
+  }
+
+
 
 }
