ef76153aaa1f88cf638d0af9d5942dd4149e3517
Dipak Kumar Nimawat <dipak.nimawat@gmail.com>
2018-02-26 00:22:16 +0530

    form vaidation

--- src/app/validate-fields-submit-form/validate-fields-submit-form.component.ts
+++ src/app/validate-fields-submit-form/validate-fields-submit-form.component.ts
@@ -19,15 +19,40 @@ export class ValidateFieldsSubmitFormComponent implements OnInit {
   ngOnInit() {
     this.form = this.formBuilder.group({
       name: [null, Validators.required],
+      birth_palace: [null, Validators.required],
+      dob: [null, Validators.required],
+      birth_time: [null, Validators.required],
+      height: [null, Validators.required],
+      weight: [null, Validators.required],
+      complexion: [null, Validators.required],
+      education: [null, Validators.required],
+      profession: [null, Validators.required],
+      currently_working: [null, Validators.required],
+      hobbies: [null, Validators.required],
+      cast: [null, Validators.required],
+      sub_cast: [null, Validators.required],
+      gotra: [null, Validators.required],
+      maththab: [null, Validators.required],
+      speak_urdu: [null, Validators.required],
+      namaz: [null, Validators.required],
+      fasting: [null, Validators.required],
+      hijab_after_marriage: [null, Validators.required],
+      quran: [null, Validators.required],
+      occupation: [null, Validators.required],
+      father: [null, Validators.required],
+      mother: [null, Validators.required],
+      brother: [null, Validators.required],
+      sister: [null, Validators.required],
+      phone_no: [null, Validators.required],
       email: [null, [Validators.required, Validators.email]],
-      address: this.formBuilder.group({
+     /* address: this.formBuilder.group({
         street: [null, Validators.required],
         street2: [null],
         zipCode: [null, Validators.required],
         city: [null, Validators.required],
         state: [null, Validators.required],
         country: [null, Validators.required]
-      })
+      })*/
     });
   }
 
@@ -45,7 +70,7 @@ export class ValidateFieldsSubmitFormComponent implements OnInit {
   onSubmit() {
     console.log(this.form);
     if (this.form.valid) {
-      console.log('form submitted');
+      console.log('successfully form submitted');
     } else {
       this.validateAllFormFields(this.form);
     }
