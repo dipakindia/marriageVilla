82f9eb40d30476ca12d08b7efa61cfe14b18c0c7
Dipak Kumar Nimawat <dipak.nimawat@gmail.com>
2018-03-02 22:40:06 +0530

    contact newsletter

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,69 +1,31 @@
 import { Component, OnInit ,Input } from '@angular/core';
 import {ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
-
+import { DataService } from './../data.service';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-  options:FormGroup;
-  myform: FormGroup;
-  fname: FormControl;
-  lname: FormControl;
-  email: FormControl;
-  phone: FormControl;
-  company_name:FormControl;
-  website:FormControl;
-  find_us:FormControl;
+  public news_letter_text:string = '';
+  public msg:string;
+  public success:string;
+  constructor(private formBuilder: FormBuilder, private data_service: DataService) { }
 
-  constructor(fb: FormBuilder) {
-    this.options = fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
-    });
-  }
+  
 
   ngOnInit() {
-    this.createFormControls();
-    this.createForm();
-
   }
-  createFormControls() {
-  this.fname = new FormControl('',
-  [Validators.required]
-  );
-  this.lname = new FormControl('',
-  [Validators.required]
-  );
-  this.email = new FormControl('',
-  [Validators.required]
-  );
-  this.phone = new FormControl('',
-  [Validators.required]
-  );
-  this.company_name = new FormControl('',
-  [Validators.required]
-  );
-  this.find_us = new FormControl('',
-  [Validators.required]
-  );
-  this.website = new FormControl('',
-  [Validators.required]
-  );
-  }
-
-
-  createForm(){
-    this.myform = new FormGroup({
-      fname: this.fname,
-      lname:this.lname,
-      email: this.email,
-      phone: this.phone, 
-      company_name: this.company_name,
-      website:this.website,
-      find_us:this.find_us,
-
+  submitNewsLetter(){ 
+    this.data_service.addNewsLetter({email:this.news_letter_text}).subscribe(result=>{
+      if(result['statusCode'] == 1){
+        this.success = result['msg'];
+        this.msg = '';
+      }else{
+        this.msg = result['msg'];
+        this.success = '';
+        
+      }
     });
   }
 
